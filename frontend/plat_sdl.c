f24f5871a6b77b0fca0dc6dae641484e556a8026
Apaczer <94932128+Apaczer@users.noreply.github.com>
2025-06-10 21:33:00 +0200

    frontend/plat_sdl: reduce pitch for 239 width
    make games like "Cool Boarders 2 (USA)" not pitched half way

--- frontend/plat_sdl.c
+++ frontend/plat_sdl.c
@@ -461,7 +461,7 @@ static int adj_src_dst(const SDL_Surface *sfc, int w, int pp, int *h,
     line_w = sfc->w;
   }
   if (sfc->h > *h)
-    *dst += sfc->pitch * (sfc->h - *h) / 2 / 2;
+    *dst += sfc->pitch * (sfc->h - *h) / 2 / (*h != 239 ? 2 : 1);
   else {
     *src += pp * (*h - sfc->h) / 2;
     *h = sfc->h;
